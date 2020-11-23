8aadfe35c478a2aff2f50f67d7c0960c846e641f
Aamir S <40729965+AamirL1011@users.noreply.github.com>
2020-11-23 15:47:28 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,40 @@ import './App.css';
 import React from "react";
 
 class App extends React.Component {
+    _isMounted;
 
   constructor(props) {
     super(props);
+    this.state = {
+        patient: null,
+    }
   }
-  
-  render() {
-    const {client} = this.props;
-    console.log("client: ", client);
+
+  async componentDidMount() {
+      this._isMounted = true;
+      const {client} = this.props;
+      try {
+          let patient = await client.patient.read();
+          let observation = await client.patient.request("Observation");
+          let allergy = await client.patient.request("AllergyIntolerance");
+          console.log("patient: ", patient);
+          console.log("observation: ", observation);
+          console.log("allergy: ", allergy);
+          if (this._isMounted) {
+              this.setState({
+                  patient: patient,
+              })
+          }
+      } catch (err) {
+          console.log("Error requesting FHIR resources: ", err);
+      }
+  }
+
+  componentWillUnmount() {
+      this._isMounted = false;
+  }
+
+    render() {
   return (
       <div className="App">
         <div>SMART ON FHIR LAUNCH SUCCESSFUL</div>
